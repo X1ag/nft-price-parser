5a608ce92600901e388aa3b8422a1854b1559b42
X1ag <83949999+X1ag@users.noreply.github.com>
2024-10-18 12:56:45 +0300

    Update parser.py

--- backend/methods/parser.py
+++ backend/methods/parser.py
@@ -5,28 +5,24 @@ import json
 import os
 from methods.get_floor import get_nft_collection_floor
 
-os.environ['TZ'] = 'Europe/Moscow'
-
+timezone = pytz.timezone('Europe/Moscow')
 prices = []
 close_price = None
 isClose = False
-now = datetime.now()
-close_time = (now + timedelta(minutes=5)).replace(minute=0, second=0, microsecond=0)
-open_time = now.replace(second=0, microsecond=0)
+now = datetime.now(timezone)
+close_time_hour = (now + timedelta(hours=1)).replace(minute=0, second=0, microsecond=0)
+open_time_hour = now.replace(minute=0,second=0, microsecond=0)
+close_time_minutes = (now + timedelta(minutes=5)).replace(second=0, microsecond=0)
+open_time_minutes = now.replace(second=0, microsecond=0)
 prices = []  
 
-def set_timezone():
-    # Устанавливаем временную зону с использованием pytz
-    timezone = pytz.timezone('Europe/Moscow')
-    now = datetime.now(timezone)
-    print(f'Current time in Europe/Moscow timezone: {now}')
-
-def get_time(address):
-    global open_time, close_time, prices
+def get_time_minutes(address):
+    global open_time_minutes, close_time_minutes, prices
+    
     # Обновление текущего времени
-    now = datetime.now()
-    if len(prices) > 0 and close_time <= now.replace(second=0, microsecond=0):
-        filename = f'./candles/candleHistory{address}.json'
+    now = datetime.now(timezone)
+    if len(prices) > 0 and close_time_minutes <= now.replace(second=0, microsecond=0):
+        filename = f'./candles/candleHistory{address}5m.json'
         if os.path.exists(filename): 
             with open(filename, 'r+') as json_file:
                 json_data = json.load(json_file)
@@ -34,8 +30,8 @@ def get_time(address):
             json_data = {"data": []}
 
         data = {
-            'openTime': int(open_time.timestamp() * 1000),
-            'closeTime': int(close_time.timestamp() * 1000),
+            'openTime': int(open_time_minutes.timestamp() * 1000),
+            'closeTime': int(close_time_minutes.timestamp() * 1000),
             'percentChangePrice': percentChange(),
             'currentPrice': prices[-1],
             'open': prices[0],
@@ -48,23 +44,61 @@ def get_time(address):
         # Записываем обновленные данные в файл
         with open(filename, 'w+') as json_file:
             json.dump(json_data, json_file, indent=4)
-        print(f"File \033[96mcandlesANON\033[0m.json updated, request amount: {len(prices)}")
+        print(f"File \033[96mcandlesHistory{address}5m\033[0m.json updated, request amount: {len(prices)}")
         prices.clear()
-        close_time = (now + timedelta(minutes=5)).replace(second=0, microsecond=0)
-        open_time = now.replace(second=0, microsecond=0)
+        close_time_minutes = (now + timedelta(minutes=5)).replace(second=0, microsecond=0)
+        open_time_minutes = now.replace(second=0, microsecond=0)
 
-    print(f'\033[92m close time: {close_time} \033[0m')
-    return open_time, close_time
+    print(f'\033[92m close time: {close_time_minutes} \033[0m')
+    return open_time_minutes, close_time_minutes
+
+def get_time_hour(address):
+    global open_time_hour, close_time_hour, prices
+    # Обновление текущего времени
+    now = datetime.now(timezone)
+    if len(prices) > 0 and close_time_hour <= now.replace(minute=0,second=0, microsecond=0):
+        filename = f'./candles/candleHistory{address}1h.json'
+        if os.path.exists(filename): 
+            with open(filename, 'r+') as json_file:
+                json_data = json.load(json_file)
+        else:
+            json_data = {"data": []}
+
+        data = {
+            'openTime': int(open_time_hour.timestamp() * 1000),
+            'closeTime': int(close_time_hour.timestamp() * 1000),
+            'percentChangePrice': percentChange(),
+            'currentPrice': prices[-1],
+            'open': prices[0],
+            'high': max(prices),
+            'low': min(prices),
+            'close': prices[-1],
+        }
+        # Добавляем новые данные в список "data"
+        json_data["data"].append(data)
+        # Записываем обновленные данные в файл
+        with open(filename, 'w+') as json_file:
+            json.dump(json_data, json_file, indent=4)
+        print(f"File \033[96mcandlesHistory{address}\033[0m.json updated, request amount: {len(prices)}")
+        prices.clear()
+        close_time_hour = (now + timedelta(hours=1)).replace(minute=0,second=0, microsecond=0)
+        open_time_hour = now.replace(minute=0,second=0, microsecond=0)
+
+    print(f'\033[92m close time: {close_time_hour} \033[0m')
+    return open_time_hour, close_time_hour
 
 async def getPrice(address):
     print(f'Fetching price for address: {address}')
     result = await get_nft_collection_floor(address)
+    if result is None:
+        asyncio.sleep(15)
+        result = await get_nft_collection_floor(address)
     prices.append(result)
     print(f'\033[92m Price fetched: {result} \033[0m')
     return result
 
 def percentChange():
-    if len(prices) < 2:
+    if len(prices) < 2 or prices[0] is None or prices[-1] is None:
         return None
     return ((prices[-1] - prices[0]) / (prices[0] + prices[-1] / 2)) * 100
 
@@ -78,8 +112,8 @@ async def getData(address):
     while True:
         try:
             data = {
-                'openTime': int(get_time(address)[0].timestamp() * 1000),
-                'closeTime': int(get_time(address)[1].timestamp() * 1000),
+                'openTime': int(get_time_hour(address)[0].timestamp() * 1000),
+                'closeTime': int(get_time_hour(address)[1].timestamp() * 1000),
                 'percentChangePrice': percentChange(),
                 'currentPrice': await getPrice(address),
                 'open': prices[0],
@@ -95,10 +129,9 @@ async def getData(address):
         await asyncio.sleep(5)
 
 async def main(address):
-    set_timezone()
     print('Starting main function')
     await getData(address)
 
 if __name__ == "__main__":
-    address = "EQBcjALtmHwSBCSpDOZ1_emrSQVtJU6J0POZR-ThkZjfXkZs"
+    address = "EQAOQdwdw8kGftJCSFgOErM1mBjYPe4DBPq8-AhF6vr9si5N"
     asyncio.run(main(address))
