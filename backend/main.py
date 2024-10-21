38e856101dca642584feeb6d48d4ae85623896ef
X1ag <super.don17080@gmail.com>
2024-10-21 17:21:54 +0300

    Add new route, which return full info about nft by address

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ def start_parser(address):
 def start_main():
 	asyncio.run(api.main())
  
-thread1 = Thread(target=start_parser, args=("EQBcjALtmHwSBCSpDOZ1_emrSQVtJU6J0POZR-ThkZjfXkZs",), daemon=True)
+thread1 = Thread(target=start_parser, args=("EQAOQdwdw8kGftJCSFgOErM1mBjYPe4DBPq8-AhF6vr9si5N",), daemon=True)
 thread1.start()
 start_main()
 print('script started')
--- backend/methods/api.py
+++ backend/methods/api.py
@@ -1,6 +1,7 @@
 import json
 from quart import Quart, jsonify
 from quart_cors import cors
+import requests
 
 app = Quart(__name__)
 app = cors(app, allow_origin='http://92.205.129.142:8080, http://localhost:4200')
@@ -33,5 +34,19 @@ async def get_history(address):
 async def health():
     return "Health check: OK \n"
 
+@app.route('/dyweapi/v1/getCollectionInfo/<address>', methods=['GET'])
+async def get_collection_info(address: str = 'EQAOQdwdw8kGftJCSFgOErM1mBjYPe4DBPq8-AhF6vr9si5N' ):
+    request = requests.get(f'https://tonapi.io/v2/nfts/collections/{address}').json()["metadata"]
+    symbol = ''
+    if len(request["name"].split(' ')) > 1:
+        for i in range(len(request["name"])+1):
+            symbol += request["name"].split(" ")[i-1][0] 
+    request.update({
+        "symbol": symbol
+        })
+    if "description" in request:
+        del request["description"]
+    return request
+            
 def main():
     app.run(debug=True)
